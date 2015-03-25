9e44bc957de54f16f59dd3706dcfd9a8c36a7436
Johan Buys <johan@appbase.co.za>
2015-03-26 00:14:53 +0200

    fixed slash
    replaced / with windows \

--- deploy.sh
+++ deploy.sh
@@ -88,7 +88,7 @@ curl -sS https://getcomposer.org/installer | php
  
 # Invoke Composer in the deployment directory
 echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
-php -d extension=php_intl.dll $DEPLOYMENT_TARGET/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
+php -d extension=php_intl.dll $DEPLOYMENT_TARGET\composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
 # Post deployment stub
 if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
   POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
